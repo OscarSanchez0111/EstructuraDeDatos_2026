4cf2ba2bf109eb1cb8df924e7ee8e8c93af5afd1
OscarSanchez0111 <oscar.xo.ps@outlook.com>
2026-05-28 20:41:23 -0600

    Clase 1: Implementación de funciones y lógica de polígonos

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int lados = SeleccionarPoligono();
+
+        double[] datos = PedirDatos();
+
+        double area = CalcularArea(lados, datos[0], datos[1]);
+
+        Console.WriteLine("\nEl área del polígono es: " + area);
+    }
+
+    static int SeleccionarPoligono()
+    {
+        Console.WriteLine("Seleccione un polígono:");
+        Console.WriteLine("1. Pentágono");
+        Console.WriteLine("2. Hexágono");
+        Console.WriteLine("3. Heptágono");
+
+        int opcion = Convert.ToInt32(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                return 5;
+
+            case 2:
+                return 6;
+
+            case 3:
+                return 7;
+
+            default:
+                Console.WriteLine("Opción inválida.");
+                return 0;
+        }
+    }
+
+    static double[] PedirDatos()
+    {
+        Console.Write("\nIngrese la medida del lado: ");
+        double lado = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese la apotema: ");
+        double apotema = Convert.ToDouble(Console.ReadLine());
+
+        return new double[] { lado, apotema };
+    }
+
+    static double CalcularArea(int lados, double lado, double apotema)
+    {
+        double perimetro = lados * lado;
+
+        double area = (perimetro * apotema) / 2;
+
+        return area;
+    }
+}
