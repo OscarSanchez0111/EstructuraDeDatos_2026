e2708e42bc0c6bf31ce7d2e9112a7b6ec942826d
OscarSanchez0111 <oscar.xo.ps@outlook.com>
2026-05-28 21:27:49 -0600

    Clase 4: Implementación de algoritmos recursivos (Factorial y Fibonacci) con validaciones

---
+++ Clase4_Recursividad/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un número: ");
+
+        string entrada = Console.ReadLine();
+        int numero = int.Parse(entrada);
+
+        long resultado = CalcularFactorial(numero);
+    
+
+        Console.WriteLine("El factorial es: " + resultado);
+    }
+
+    static long CalcularFactorial(int n)
+    {
+        // Validación de entrada
+        if (n < 0)
+            throw new ArgumentException(
+            "No existe factorial de negativos.");
+
+        // Caso Base
+        if (n == 0 || n == 1)
+            return 1;
+
+        // Caso Recursivo
+        return n * CalcularFactorial(n - 1);
+    }
+}
